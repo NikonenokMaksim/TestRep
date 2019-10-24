a24f28160774bb4c3eb26a32f54d1e96d6e91e82
Maksim <nikonenok1987@inbox.ru>
2019-10-24 15:36:30 +0300

    Добавил CSWI

---
+++ CSWI/CSWI_SG2.m
@@ -0,0 +1,14 @@
+BlkTmms1=650;%
+BlkTmms2=750;%
+ClsTmms1=200;%
+ClsTmms2=100;%
+ClsTmms3=30;%
+ClsTmms4=20;%
+FailTmms=300;%
+FctMod1=1;
+FctMod2=1;
+FctMod3=1;
+OpnTmms1=250;%
+OpnTmms2=150;%
+OpnTmms3=90;%
+
---
+++ CSWI/CSWI_SG5.m
@@ -0,0 +1,14 @@
+BlkTmms1=700;
+BlkTmms2=800;
+ClsTmms1=250;
+ClsTmms2=170;
+ClsTmms3=60;
+ClsTmms4=10;
+FailTmms=350;
+FctMod1=1;
+FctMod2=1;
+FctMod3=0;%
+OpnTmms1=300;
+OpnTmms2=200;
+OpnTmms3=30;
+
